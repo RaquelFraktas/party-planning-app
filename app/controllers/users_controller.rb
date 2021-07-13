96c09b28fc7fce5770ef8564e0a9865bc53050c2
github username <github email address>
2021-07-12 22:52:14 -0400

    add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+  
+  def user_params
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :themes
   resources :parties
   resources :users
+
+  root "sessions#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
